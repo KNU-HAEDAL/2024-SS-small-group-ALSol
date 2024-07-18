f3a14879f321dd379427c758a319b249a1a1fa2f
park jiyeong <qkrwldud4690@naver.com>
2024-07-18 15:05:22 +0900

    29수정

--- JYPARK/21to30/29.py
+++ JYPARK/21to30/29.py
@@ -1,34 +1,30 @@
 def solution(enroll, referral, seller, amount):
     answer = []
     tree_table = { }
-    seller_table = { }
-    money_talbe = { }
+    money_talbe = {name : 0 for name in enroll}
+
     #tree_table 작성 (key : 추천 받은 자, value : 추천 해준 자)
     for i in range(len(enroll)):
-        money_talbe[enroll[i]] = 0
-        if referral[i] != '-':
-            tree_table[enroll[i]] = referral[i]
+        tree_table[enroll[i]] = referral[i]
+
+    #돈 계산 함수 (While True 대체)
+    def culculate_profit(person, money):
+        if person == '-' or money < 1:
+            return 0
         else:
-            tree_table[enroll[i]] = '-'
+            money_talbe[person] += money - money//10
+            money = money//10
+            culculate_profit(tree_table[person], money) 
 
     for j in range(len(seller)):
-        money = int(amount[j] * 100)
-        while True:
-            #추천자가 없는 경우
-            if tree_table[seller[j]] == '-':
-                money_talbe[seller[j]] += money - int(money * 0.1)
-                break
-            #추천자가 있는 경우
-            else:
-                money_talbe[seller[j]] += int(money - money * 0.1)
-                seller[j] = tree_table[seller[j]]
-                money = money * 0.1
-    for k in money_talbe:
-        answer.append(int(money_talbe[k]))   
+        money = amount[j] * 100
+        culculate_profit(seller[j], money)
+       
+    answer = [money_talbe[person] for person in money_talbe]
     return answer
 
 enroll = ["john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"]
 referral = ["-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"]
-seller = ["sam", "emily", "jaimie", "edward"]
-amount = [2, 3, 5, 4]
+seller = ["young", "john", "tod", "emily", "mary"]
+amount = [12, 4, 2, 5, 10]
 print(solution(enroll, referral, seller, amount))
