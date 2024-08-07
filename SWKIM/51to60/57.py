f76e45ff198785e79fa7d06b0d143ea4edc3f497
seongwon02 <seongwon2303@gmail.com>
2024-08-07 17:52:04 +0900

    57 완

--- SWKIM/51to60/57.py
+++ SWKIM/51to60/57.py
@@ -1,6 +1,6 @@
 def solution(n):
     # n을 문자열로 바꾼 후 정수를 기준으로 내림차순 정렬
     answer = sorted(str(n), key=lambda x: int(x), reverse=True)
-    return "".join(answer)
+    return int("".join(answer))
 
 print(solution(118372))
