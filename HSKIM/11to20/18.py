e30d1c529f5bcfa8fe49234f0aa57bf7c62acfe8
huisuu <kimhuisu005791@gmail.com>
2024-08-12 20:28:18 +0900

    18.py

--- HSKIM/11to20/18.py
+++ HSKIM/11to20/18.py
@@ -1,10 +1,11 @@
 def solution(arr, target):
     l = len(arr)
     for i in range(0, l-1):
-        for j in range(i + 1, l): 
-            print(arr[i] + arr[j], target)
-            if (arr[i] + arr[j]) == target:
-                return True
+        if i <= target:
+            for j in range(i + 1, l): 
+                print(arr[i] + arr[j], target)
+                if (arr[i] + arr[j]) == target:
+                    return True
     return False
 
 # print(solution([1,2,3,4,8], 6))
