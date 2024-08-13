09039b2abc3088ed597d9a98a9f4810a436d5a47
seongwon02 <seongwon2303@gmail.com>
2024-08-13 17:53:41 +0900

    73 완

---
+++ SWKIM/71to80/73.py
@@ -0,0 +1,11 @@
+def solution(n):
+    fibo = [0, 1]
+
+    # fibo(n) = fibo(n-2) + fibo(n-1)을 활용
+    # 재귀 사용시 메모리 초과
+    for i in range(2, n+1):
+        fibo.append((fibo[i-2] + fibo[i-1]) % 1234567)
+    
+    return fibo[-1]
+
+print(solution(5))
