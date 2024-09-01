4afa482b04fa323e4fb3e5fc71097ffe90c9e590
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2024-09-01 09:20:46 +0900

    delete empty files

--- JEOH/71to80/80.py
+++
@@ -1,9 +0,0 @@
-def solution(amount):
-  denominations = [1, 10, 50, 100]
-  denominations.sort(reverse=True) 
-  change = [] 
-  for coin in denominations:
-    while amount >= coin: 
-        change.append(coin) 
-        amount -= coin  
-  return change
